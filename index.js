636acc5ef18b9e5c5aa06110c76adabd8680f2c8
roccorizz <sknahid68@gmail.com>
2023-04-01 16:00:50 +0600

    database connected

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const colors = require('colors');
+const cors = require('cors');
+require('dotenv').config();
+
+
+const connectDB = require('./config/db');
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Connect to database
+connectDB();
+
+app.use(cors());
+
+
+app.get('/', (req, res) => {
+    res.send('Server is running');
+})
+app.listen(port, console.log(`Server running on port ${port}`));
+

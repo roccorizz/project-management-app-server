d910d4c4f65da2f53f0193d62d6fcfd6e408d1ca
roccorizz <sknahid68@gmail.com>
2023-04-01 16:34:48 +0600

    graphql added with sampleData

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const colors = require('colors');
 const cors = require('cors');
 require('dotenv').config();
-
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema/schema');
 
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
@@ -14,6 +15,13 @@ connectDB();
 
 app.use(cors());
 
+app.use(
+    '/graphql',
+    graphqlHTTP({
+        schema,
+        graphiql: process.env.NODE_ENV === 'development',
+    })
+);
 
 app.get('/', (req, res) => {
     res.send('Server is running');

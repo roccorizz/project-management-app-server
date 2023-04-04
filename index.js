9c5b4782216b5ac8d3d46d624ae6116b3655eda1
roccorizz <sknahid68@gmail.com>
2023-04-04 13:45:25 +0600

    server bugs testing second

--- index.js
+++ index.js
@@ -1,44 +1,36 @@
 const express = require('express');
-const colors = require('colors');
-const cors = require('cors');
-require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
+const cors = require('cors');
+const colors = require('colors');
+const dotenv = require('dotenv');
 const schema = require('./schema/schema');
 const connectDB = require('./config/db');
-const port = process.env.PORT || 5000;
 
+dotenv.config();
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Connect to database
 connectDB();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
-    next()
-})
 
-
-
+// Enable CORS
 app.use(cors());
+
+// Parse JSON body
 app.use(express.json());
 
+// GraphQL endpoint
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === 'development',
+}));
+
+// Test route
 app.get('/', (req, res) => {
     res.send('Server is running');
-})
+});
 
-
-app.use(
-    '/graphql',
-    graphqlHTTP({
-        schema,
-        graphiql: process.env.NODE_ENV === 'development',
-    })
-);
-
-
-
+// Start server
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
